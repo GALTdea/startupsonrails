c71bc3e624ba45b5a5d0124499a9b614ee9d732d
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-25 15:42:47 -0700

    adds devise line to spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
+
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
