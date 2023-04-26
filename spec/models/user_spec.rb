fc8fdc77900b9fad4876325b1450add54e2aa369
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-26 11:12:50 -0700

    Add user_spec.rb to test user model and devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+  end
+
+  context 'Devise authentication' do
+    let(:user) { create(:user) }
+
+    describe 'valid attributes' do
+      it 'is valid with valid attributes' do
+        expect(user).to be_valid
+      end
+
+      it 'is not valid without an email' do
+        user.email = nil
+        expect(user).not_to be_valid
+      end
+
+      it 'is not valid without a password' do
+        user.password = nil
+        expect(user).not_to be_valid
+      end
+
+      it 'is not valid with a duplicate email' do
+        new_user = build(:user, email: user.email)
+        expect(new_user).not_to be_valid
+      end
+    end
+
+    describe 'sign in' do
+      it 'can sign in with valid credentials' do
+        expect(user.valid_password?(user.password)).to be_truthy
+      end
+
+      it 'cannot sign in with invalid credentials' do
+        expect(user.valid_password?('invalid_password')).to be_falsey
+      end
+    end
+  end
 end
