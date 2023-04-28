634ea09ba5ad5a4e2127acf7df59e81633211095
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-28 10:49:35 -0700

    refactors syntax in  user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validations' do
-    it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email).case_insensitive }
-    it { should validate_presence_of(:password) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:password) }
   end
 
   context 'Devise authentication' do
