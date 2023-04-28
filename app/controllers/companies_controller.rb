cc020ee947028b7b3c552f40f9c63892c03e938c
Gustavo Valenzuela <GALTdea@gmail.com>
2023-04-28 11:18:33 -0700

    add basic company form and new view

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,5 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -6,8 +7,32 @@ class CompaniesController < ApplicationController
   end
 
   def new
+    @company = Company.new
+  end
+
+  def create 
+    @company = Company.new(company_params)
+    @company.user_id = current_user.id
+    respond_to do |format|
+      if @company.save
+        format.html { redirect_to @company, notice: "Thank you for your submission! The company's profile will be activated upon review." }
+        # redirect_to @company
+      else
+        render :new
+      end
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :url, :email)
   end
 end
