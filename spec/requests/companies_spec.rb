572e340079bdbd968f6bd634af8f993f7accee98
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-08-17 18:28:37 -0700

    WIP Starts companies_spec, still ongoing

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
+  let(:user) { create(:user) }
+  let(:admin){ create(:admin)}
+  let(:company) { create(:company) }
+   
+  
   describe "GET /index" do
     it "returns http success" do
       get "/companies/index"
@@ -8,15 +13,21 @@ RSpec.describe "Companies", type: :request do
     end
   end
 
+
+
+  
   describe "GET /show" do
+
     it "returns http success" do
-      get "/companies/show"
+     company = create(:company)
+      get "/companies/#{company.id}}"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /new" do
     it "returns http success" do
+    
       get "/companies/new"
       expect(response).to have_http_status(:success)
     end
