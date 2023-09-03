cb0780e4dc707fe3aa6d74e328f10531aea116b0
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-02 17:18:45 -0700

    adds category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
+    @categories = Category.all
   end
 
   def show
@@ -13,7 +14,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    binding.pry
+   @category = Category.new(category_params)
     if @category.save 
       redirect_to @category, notice: "Category was successfully created."
     else
@@ -28,6 +29,10 @@ class CategoriesController < ApplicationController
 
   def set_category
     @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit( :name, :description)
   end
 
 end
