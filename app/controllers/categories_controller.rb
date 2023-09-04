269691695dd4accedb8d2175b2b7775bb3d2128e
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-03 17:40:32 -0700

    comments out authentication

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
