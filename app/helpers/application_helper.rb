0a1218bbe3ac8cdcaa8c0e30d9bbc19723b886ae
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-07 20:04:32 -0700

    Adds generate_svg_with_initials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def generate_svg_with_initials(name)
+    initials = name[0..1].upcase
+    svg_html = <<-SVG
+      <svg width="50" height="50" class="company-logo">
+        <rect width="100%" height="100%" fill="#eee" />
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24px" font-family="Arial">
+          #{initials}
+        </text>
+      </svg>
+    SVG
+    svg_html.html_safe
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+
+  def initials
+    self.name.split.map(&:first).join
+  end
 end
