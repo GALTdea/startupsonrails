d3c5b9c9e06278048997bd115734b9bbf5626c7b
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-11 10:00:10 -0700

    modify generate_svg_with_initials method to be more responsive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
   def generate_svg_with_initials(name)
     initials = name[0..1].upcase
     svg_html = <<-SVG
-      <svg width="50" height="50" class="company-logo">
+      <svg width="100" height="100" class="company-logo">
         <rect width="100%" height="100%" fill="#eee" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="24px" font-family="Arial">
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Arial">
           #{initials}
         </text>
       </svg>
