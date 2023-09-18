4feef8ff8bd1fc61b8264a161aedddcd2fa21eec
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-18 12:24:28 -0700

    made generate_svb_with_initials method adaptable to itps parent container

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,14 @@ module ApplicationHelper
   def generate_svg_with_initials(name)
     initials = name[0..1].upcase
     svg_html = <<-SVG
-      <svg width="100" height="100" class="company-logo">
-        <rect width="100%" height="100%" fill="#eee" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Arial">
+      <svg width="100%" height="100%" viewBox="0 0 100 100" class="company-logo">
+        <rect width="100" height="100" fill="#eee" />
+        <text x="50" y="50" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Arial">
           #{initials}
         </text>
       </svg>
     SVG
     svg_html.html_safe
   end
+  
 end
