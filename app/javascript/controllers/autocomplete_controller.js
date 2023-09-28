3acd15f2d489a9af84e9ca8f413393c7b40f337e
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-28 12:50:31 -0700

    remvoes line under search box when dropdown is not been used

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -27,7 +27,20 @@ export default class extends Controller {
     const companyId = event.currentTarget.getAttribute('data-id');
     window.location.href = `/companies/${companyId}`;
   }
+
+  haveResults() {
+    return this.resultsTarget.children.length > 0;
+  }
+
+  populateResults(data) {
+    // populate the results
+
+  
+    if (this.haveResults()) {
+      this.resultsTarget.classList.add('has-results');
+    } else {
+      this.resultsTarget.classList.remove('has-results');
+    }
+  }
 }
-
-
 
