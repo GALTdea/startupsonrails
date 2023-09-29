286ff80ce00dd5ffe3e9dd026c800a61c2c9f479
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-29 11:44:20 -0700

    changes company svg size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
   def generate_svg_with_initials(name)
     initials = name[0..1].upcase
     svg_html = <<-SVG
-      <svg width="100%" height="100%" viewBox="0 0 100 100" class="company-logo">
-        <rect width="100%" height="100%" fill="#eee" />
+      <svg width="70" height="70" viewBox="0 0 100 100" class="company-logo  ">
+        <rect width="100%" height="100%" fill="#007bff" />
         <text x="50" y="50" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Arial">
           #{initials}
         </text>
@@ -14,3 +14,4 @@ module ApplicationHelper
     svg_html.html_safe
   end
 end
+
