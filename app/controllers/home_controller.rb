b56aca2f01da980a32a22ad7a27042ca10024896
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-09-30 12:54:34 -0700

    linting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,9 @@ class HomeController < ApplicationController
   def show
     @categories = Category.showcased
 
-
     @companies_by_category = {}
-
     @categories.each do |category|
       @companies_by_category[category.name] = category.companies
     end
-    
   end
 end
