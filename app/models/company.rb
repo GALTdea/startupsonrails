a96ba61a9dd1aaeec7e0532c0d836c211b33b8bf
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-10-09 13:15:41 -0700

    Refactors import_from_csv method

--- app/models/company.rb
+++ app/models/company.rb
@@ -31,37 +31,45 @@ class Company < ApplicationRecord
   enum status: { pending: 0, active: 1, rejected: 2 }
 
   def self.import_from_csv(current_user)
-    csv_file_path = Rails.root.join('data', 'companies-5.csv')
-
+    csv_file_path = Rails.root.join('data', 'companies-6.csv')
+  
     success_count = 0
     error_count = 0
     error_messages = []
-
+  
     allowed_attributes = Company.new.attributes.keys.map(&:to_sym) # Get all allowed attributes
-
+  
     CSV.foreach(csv_file_path, headers: true, header_converters: :symbol) do |row|
       row_hash = row.to_h
-
+  
       # Keep only the allowed attributes
       filtered_row_hash = row_hash.slice(*allowed_attributes)
-
+  
       # Add the user_id to the attributes
       filtered_row_hash[:user_id] = current_user.id
-
+  
       company = self.new(filtered_row_hash)
-
-      if company.save
-        success_count += 1
-      else
+  
+      begin
+        if company.save
+          success_count += 1
+        else
+          error_count += 1
+          error_messages << company.errors.full_messages.join(', ')
+        end
+      rescue ActiveRecord::RecordNotFound => e
         error_count += 1
-        error_messages << company.errors.full_messages.join(', ')
+        error_message = "Failed to save or find company named '#{filtered_row_hash[:name]}': #{e.message}"
+        error_messages << error_message
+        Rails.logger.error(error_message)
       end
     end
-
+  
     {
       success_count: success_count,
       error_count: error_count,
       error_messages: error_messages
     }
   end
+  
 end
