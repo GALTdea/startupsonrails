5b8e7511fa09fc411fe524eba282a99c044428e1
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-10-10 10:35:38 -0700

    Adds top_rails_category to category_names array

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :categorizations
   has_many :companies, through: :categorizations
 
-  category_names = ["cool_cats", "hotwire", "trending", "up_and_comers"]
+  category_names = ["top_rails_companies", "cool_cats", "hotwire", "trending", "up_and_comers"]
   scope :showcased, -> { where(name: category_names) }
 end
