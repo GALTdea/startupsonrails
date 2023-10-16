c4dd1a906ccd4701b44e9acf9a807aa259fb82ee
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-10-16 09:43:45 -0700

    refactors generate_svg_with_initials helper to take options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def generate_svg_with_initials(name)
+  def generate_svg_with_initials(name, classes='', height='', width='')
     initials = name[0..1].upcase
     svg_html = <<-SVG
-      <svg width="46" height="46" viewBox="0 0 100 100" class="company-logo align-middle rounded shadow-md">
-        <rect width="100%" height="100%"  />
+      <svg width="#{width}" height="#{height}" viewBox="0 0 100 100" class="company-logo align-middle rounded shadow-md #{classes}">
+        <rect width="100%" height="100%" />
         <text x="50" y="50" dominant-baseline="middle" text-anchor="middle" font-size="3em" font-family="Arial">
           #{initials}
         </text>
