ee9fd9f8ddef7e6a43a61bfcbc240589e42a3eff
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-10-20 11:12:15 -0700

    Adds tops-listings to home page

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,7 @@ class Category < ApplicationRecord
 
   category_names = ["top_rails_companies", "trending", "hotwire", "tooling"]
   scope :showcased, -> { where(name: category_names) }
+
+  tops_categories = ["top_bootstraped", "top_contribuitors", "top_rails_companies", "top_hotwire_use", "top_jr_friendly", "top_tooling", "top_indy"]
+  scope :tops_categories, -> { where(name: tops_categories) }
 end
