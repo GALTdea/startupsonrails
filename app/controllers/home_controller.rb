d1d1c8a2e01db7295147a3adf93557a2661ffe74
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-11-06 11:20:31 -0800

    restore home controller and view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,16 @@
 class HomeController < ApplicationController
   def show
-    # @top_categories = Category.showcased
-    # @companies_by_category = {}
-    # @top_categories.each do |category|
-    #   @companies_by_category[category.name] = category.companies
-    # end
+    @top_categories = Category.showcased
+    @companies_by_category = {}
+    @top_categories.each do |category|
+      @companies_by_category[category.name] = category.companies
+    end
 
-    # @tops_categories = Category.tops_categories
-    # @tops_companies = {}
-    # Category::TOPS_CATEGORIES_HASH.each do |category, name|
-    #   cat = Category.find_by(name: category)
-    #   @tops_companies[name] = cat.companies
-    # end
+    @tops_categories = Category.tops_categories
+    @tops_companies = {}
+    Category::TOPS_CATEGORIES_HASH.each do |category, name|
+      cat = Category.find_by(name: category)
+      @tops_companies[name] = cat.companies
+    end
   end
 end
