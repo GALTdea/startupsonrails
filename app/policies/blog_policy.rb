05ed66be5c6c195b5798ac76e077b3a44e9f8bae
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-12-21 12:32:34 -0800

    install pundit and add blog policy

---
+++ app/policies/blog_policy.rb
@@ -0,0 +1,20 @@
+class BlogPolicy < ApplicationPolicy
+
+  def index?
+    !user.nil?
+  end
+
+  def new?
+    !user.nil?
+  end
+
+
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      if user.present?
+        scope.all
+      end
+    end
+  end
+end
