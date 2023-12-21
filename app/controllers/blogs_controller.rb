cae7760653d584c06b64a7b12d15dac8ff0e0be4
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-12-21 13:06:09 -0800

    Add blog new and create functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,10 +7,25 @@ class BlogsController < ApplicationController
   end
 
   def new
-    authorize :blog
+    @blog = authorize Blog.new
+  end
+
+  def create
+    @blog = authorize Blog.new(blog_params)
+    @blog.user = current_user
+    if @blog.save
+      redirect_to @blog, notice: "Blog was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
     authorize :blog
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :body)
   end
 end
