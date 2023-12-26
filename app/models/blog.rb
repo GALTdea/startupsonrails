324b9071b7c9fda895b9c978197e3f200d6b28f2
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2023-12-26 14:28:37 -0800

    Add method to upload blog images to it's folder on s3

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,16 @@ class Blog < ApplicationRecord
   has_rich_text :content
 
   has_one_attached :image
+  before_save :set_image_key, if: :image_attached?
+  private
+
+  def image_attached?
+    image.attached?
+  end
+
+  def set_image_key
+    if image.attached?
+      image.blob.update!(key: "blog-images/#{SecureRandom.uuid}-#{image.filename}")
+    end
+  end
 end
