1eb56e487742eb4f390a2ab6e1125be35298a6e2
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-01-10 14:30:16 -0800

    Remove dummy category lists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,17 @@
 class HomeController < ApplicationController
   def show
     ahoy.track "My first event", language: "Ruby"
+
     @top_categories = Category.showcased
     @companies_by_category = {}
+
     @top_categories.each do |category|
       @companies_by_category[category.name] = category.companies
     end
 
     @tops_categories = Category.tops_categories
     @tops_companies = {}
+
     Category::TOPS_CATEGORIES_HASH.each do |category, name|
       cat = Category.find_by(name: category)
       @tops_companies[name] = cat.companies
