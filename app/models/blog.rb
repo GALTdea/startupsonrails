e19b7534cf17bc48d013aa56a986a88adb85bc98
Gustavo Valenzuela <gustavo.devvv@gmail.com>
2024-01-11 13:03:44 -0800

    WIP: initial  blog feature implementation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,9 @@ class Blog < ApplicationRecord
   has_rich_text :content
 
   has_one_attached :image
+
+  enum status: { draft: 0, published: 1, featured: 2 }
+
+
+  FEATURED = Blog.where(status: :featured).order(created_at: :desc).limit(3)
 end
