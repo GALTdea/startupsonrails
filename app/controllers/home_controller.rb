8e1acbbd0bd0ddeacce7f3e710f15bec861c3594
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-04-27 15:45:30 -0700

    Adds a @featured_companies var in home controller show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
   def show
     ahoy.track "My first event", language: "Ruby"
 
-    @featured_companies = Category.where(name: 'top_rails_companies').first.companies.limit(3)
-    @top_categories = Category.showcased
-    @companies_by_category = {}
+    @featured_companies = Company.where(name: ['Airbnb', 'Basecamp', 'bloomberg'])
+    # @top_categories = Category.showcased
+    # @companies_by_category = {}
 
-    @top_categories.each do |category|
-      @companies_by_category[category.name] = category.companies
-    end
+    # @top_categories.each do |category|
+    #   @companies_by_category[category.name] = category.companies
+    # end
 
     @tops_categories = Category.tops_categories
     @tops_companies = {}
