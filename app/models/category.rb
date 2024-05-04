ecd875b72b8ee13cf5835039576c60c43b9b7e3d
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-05-04 13:38:44 -0700

    Adds top_cities scope and top_cities var

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,16 +5,22 @@ class Category < ApplicationRecord
   category_names = ["top_rails_companies", "trending", "hotwire", "tooling"]
   tops_categories = ["top_rails_companies", "top_contribuitors", "top_hotwire_use", "top_jr_friendly", "top_tooling", "top_indy"]
 
+  top_cities = ["top_la", "top_ny"]
+
   TOPS_CATEGORIES_HASH = {
-    'top_rails_companies' => '2023 Top Companies',
+    # 'top_rails_companies' => '2023 Top Companies',
     # 'top_contribuitors' => 'Top Contribuitors',
     # 'top_hotwire_use' => 'Top Hotwire Use',
     # 'top_jr_friendly' => 'Top Jr Friendly',
     # 'top_tooling' => 'Top Tooling',
     # 'top_indy' => 'Top Indy'
+    'top_la' => 'Los Angeles',
+    'top_ny' => 'New York',
   }.freeze
+
 
 
   scope :showcased, -> { where(name: category_names) }
   scope :tops_categories, -> { where(name: tops_categories) }
+  scope :top_cities, -> { where(name: top_cities ) }
 end
