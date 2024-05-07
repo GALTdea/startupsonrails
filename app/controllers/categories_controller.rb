751a5563fe6eef52fb157299bd5926ab6cd7202a
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-05-07 13:01:34 -0700

    uncomment before_action :authenticate_user in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @categories = Category.all
