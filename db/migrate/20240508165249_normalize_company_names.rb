1eb798d8ff352e82e30845a8312f8ab124b0f8bd
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-05-08 10:03:30 -0700

    Generates migration for normalizing company name

--- db/migrate/20240508165249_normalize_company_names.rb
+++ db/migrate/20240508165249_normalize_company_names.rb
@@ -1,4 +1,8 @@
 class NormalizeCompanyNames < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    Company.find_each do |company|
+      normalized_name = company.name.downcase.strip
+      company.update(name: normalized_name)
+    end
   end
 end
