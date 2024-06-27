492825bda689dfc662c3a4a94836e65673417f6b
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-06-27 13:25:28 -0700

    imports theme js vendors to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,6 +6,29 @@ import "trix"
 import "@rails/actiontext"
 
 
-import "theme.min.js"
+import "./theme.min.js"
 
-import "./vendor/*"
+import './vendor/beStepper.js';
+import './vendor/calendar.js';
+import './vendor/chart.js';
+import './vendor/chat.js';
+import './vendor/collapse.js';
+import './vendor/countdown.js';
+import './vendor/customDragula.js';
+import './vendor/darkMode.js';
+import './vendor/datatables.js';
+import './vendor/dropzone.js';
+import './vendor/editor.js';
+import './vendor/file-upload.js';
+import './vendor/flatpickr.js';
+import './vendor/glight.js';
+import './vendor/increment-value.js';
+import './vendor/inputmask.js';
+import './vendor/jquery.min.js';
+import './vendor/pricing.js';
+import './vendor/sidebarMenu.js';
+import './vendor/tnsSlider.js';
+import './vendor/tooltip.js';
+import './vendor/typed.js';
+import './vendor/validation.js';
+// import './init_theme';
