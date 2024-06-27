ea3f6bdecfb1be7789b369b7d5550d05bbb4a414
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-06-27 13:27:07 -0700

    adds asset paths to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,11 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'theme-images')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts', 'vendor')
+Rails.application.config.assets.precompile += %w( images/theme-images/* )
+Rails.application.config.assets.precompile += %w( theme.min.css )
+Rails.application.config.assets.precompile += %w( theme-images/* )
