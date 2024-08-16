51ac889f653072f7a9c80d2b020e32de34c0e305
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-08-16 13:12:18 -0700

    Adds FEATURED_CATEGORY var, featured scope and featured_with_companies method to update popular companies component in the home page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,13 @@ class Category < ApplicationRecord
   has_many :categorizations
   has_many :companies, through: :categorizations
 
-  category_names = ["top_rails_companies", "trending", "hotwire", "tooling"]
-  tops_categories = ["top_rails_companies", "top_contribuitors", "top_hotwire_use", "top_jr_friendly", "top_tooling", "top_indy"]
+  category_names = %w[top_rails_companies trending hotwire tooling]
+  tops_categories = %w[top_rails_companies top_contribuitors top_hotwire_use top_jr_friendly top_tooling
+                       top_indy]
 
-  top_cities = ["Los Angeles", "New York", "Austin", "San Francisco", "London"]
+  top_cities = ['Los Angeles', 'New York', 'Austin', 'San Francisco', 'London']
+
+  FEATURED_CATEGORIES = %w[top_rails_companies trending hotwire tooling top_cities].freeze
 
   TOPS_CATEGORIES_HASH = {
     # 'top_rails_companies' => '2023 Top Companies',
@@ -23,26 +26,33 @@ class Category < ApplicationRecord
     # "Healthcare" => ["Company D", "Company E"],
     # "Finance" => ["Company F", "Company G", "Company H"]
 
-    "top_rails_companies" => ["shopify", "airbnb", "gitHub", "basecamp", "bloomberg", "heroku", "zendesk", "dribbble", "kickstarter", "soundcloud", "twitch", "trello", "instacart", "slack", "square", "stripe", "tinder", "uber", "venmo", "whatsapp", "zapier", "zillow", "zocdoc", "zynga"],
-    "trending" => ["beehiiv", "gorails", "gitlab", "honeybadger", "hey.com"],
-    "hotwire" => ["hey.com", "basecamp", "shopify"],
-    "tooling" => ["heroku", "github", "jumpstart"],
-    "Los Angeles" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "New York" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "Austin" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "San Francisco" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
+    'top_rails_companies' => %w[shopify airbnb gitHub basecamp bloomberg heroku zendesk dribbble
+                                kickstarter soundcloud twitch trello instacart slack square stripe tinder uber venmo whatsapp zapier zillow zocdoc zynga],
+    'trending' => ['beehiiv', 'gorails', 'gitlab', 'honeybadger', 'hey.com'],
+    'hotwire' => ['hey.com', 'basecamp', 'shopify'],
+    'tooling' => %w[heroku github jumpstart],
+    'Los Angeles' => %w[appsignal github gitlab heroku jumpstart],
+    'New York' => %w[appsignal github gitlab heroku jumpstart],
+    'Austin' => %w[appsignal github gitlab heroku jumpstart],
+    'San Francisco' => %w[appsignal github gitlab heroku jumpstart]
   }.freeze
 
   TOP_CITY_HASH = {
-    "Los Angeles" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "New York" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "Austin" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
-    "San Francisco" => ["appsignal", "github", "gitlab", "heroku", "jumpstart"],
+    'Los Angeles' => %w[appsignal github gitlab heroku jumpstart],
+    'New York' => %w[appsignal github gitlab heroku jumpstart],
+    'Austin' => %w[appsignal github gitlab heroku jumpstart],
+    'San Francisco' => %w[appsignal github gitlab heroku jumpstart]
   }.freeze
-
 
   scope :showcased, -> { where(name: category_names) }
   scope :tops_categories, -> { where(name: tops_categories) }
-  scope :top_cities, -> { where(name: top_cities ) }
+  scope :top_cities, -> { where(name: top_cities) }
+  scope :featured, -> { where(name: FEATURED_CATEGORIES) }
+
+  def self.featured_with_companies(limit = 10)
+    featured.includes(:companies).map do |category|
+      [category, category.companies.limit(limit)]
+    end.to_h
+  end
 end
 # Assuming you have a category instance called 'category' and a number of companies called 'num_companies'
