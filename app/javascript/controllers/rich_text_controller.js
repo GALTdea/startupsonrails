c97a7b2610c7a7837c141d8ccc6d01e93ef903a7
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-09-04 10:48:44 -0700

    Update rich_text_controller code

--- app/javascript/controllers/rich_text_controller.js
+++ app/javascript/controllers/rich_text_controller.js
@@ -3,5 +3,12 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="rich-text"
 export default class extends Controller {
   connect() {
+    this.resizeTextarea()
+    this.element.addEventListener('input', this.resizeTextarea.bind(this))
+  }
+
+  resizeTextarea() {
+    this.element.style.height = 'auto'
+    this.element.style.height = `${this.element.scrollHeight}px`
   }
 }
