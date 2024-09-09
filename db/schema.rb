92dd196b4de2079d82b638dac7e109eb96aaf63a
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-09-09 11:50:12 -0700

    Adds migration to add the slug column to the blogs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_09_221313) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_09_184811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_09_221313) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["title"], name: "index_blogs_on_title"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
