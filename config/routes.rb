4a2aacf12eed8a5994e9a41a97dd49cc6f999d28
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-09-17 10:56:42 -0700

    Readd update_status route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   resources :users
   resources :companies do
     get 'search', on: :collection
+    member do
+      patch :update_status
+    end
   end
 
   get 'home/show'
