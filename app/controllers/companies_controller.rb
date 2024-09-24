6fb22edaa449ba4c1ffc4e1c34d504a60715e957
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-09-24 16:07:24 -0700

    Updates twitter meta partial to process meta data from the company show page, adds content_for twitter_card_meta block to companies show page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,64 +3,6 @@ class CompaniesController < ApplicationController
   before_action :set_company, only: %i[show edit update destroy update_status]
   before_action :authenticate_user!, only: %i[new edit update destroy update_status]
 
-  # def index
-  #   if params[:query].present?
-  #     @companies = Company.where('name ILIKE ?', "%#{params[:query]}%")
-  #   elsif params[:categories].present?
-  #     # Adjust to handle multiple category IDs
-  #     category_ids = params[:categories].reject(&:blank?) # Ensure no blank IDs are processed
-  #     @pagy, @companies = pagy(Company.joins(:categories).where(categories: { id: category_ids }).distinct)
-  #   else
-  #     @pagy, @companies = pagy(Company.where(status: :active).order(name: :asc))
-  #   end
-
-  #   respond_to do |format|
-  #     format.html
-  #     format.turbo_stream
-  #     format.json do
-  #       if params[:query].present?
-  #         render json: @companies.as_json(only: %i[id name])
-  #       else
-  #         # Additional JSON response handling can be done here if needed.
-  #       end
-  #     end
-  #   end
-  # end
-
-  # def index
-  #   @companies = Company.where(status: :active)
-
-  #   if params[:query].present?
-  #     @companies = @companies.where('name ILIKE ?', "%#{params[:query]}%")
-  #   elsif params[:category_ids].present?
-  #     category_ids = params[:category_ids].reject(&:blank?)
-  #     @companies = @companies.joins(:categories).where(categories: { id: category_ids }).distinct
-  #   end
-
-  #   @pagy, @companies = pagy(@companies.order(name: :asc))
-
-  #   respond_to do |format|
-  #     format.html
-
-  #     format.turbo_stream do
-  #       render turbo_stream: turbo_stream.replace(
-  #         'companies_list',
-  #         partial: 'companies/list',
-  #         locals: { companies: @companies, pagy: @pagy }
-  #       )
-  #     end
-
-  #     format.json do
-  #       if params[:query].present?
-  #         render json: @companies.as_json(only: %i[id name])
-  #       else
-  #         # Additional JSON response handling can be done here if needed.
-  #         render json: { companies: @companies, pagy: pagy_metadata(@pagy) }
-  #       end
-  #     end
-  #   end
-  # end
-
   def index
     @companies = Company.where(status: :active)
 
@@ -91,6 +33,10 @@ class CompaniesController < ApplicationController
   end
 
   def show
+    @company = Company.friendly.find(params[:id])
+    @page_title = @company.name
+    @page_description = @company.about
+    @page_image_url = @company.logo.attached? ? url_for(@company.logo) : nil
   end
 
   def new
