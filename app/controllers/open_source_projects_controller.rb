d3d99ca26b668a58914c31531fd7fcc6078ba169
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-04 11:18:02 -0700

    Adds opensourceprojects controller actions

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,13 +1,44 @@
 class OpenSourceProjectsController < ApplicationController
+  before_action :set_company
+  before_action :set_open_source_project, only: %i[update destroy]
+
   def index
+    @open_source_projects = @company.open_source_projects
   end
 
   def create
+    @open_source_project = @company.open_source_projects.build(open_source_project_params)
+    if @open_source_project.save
+      render json: @open_source_project, status: :created
+    else
+      render json: @open_source_project.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @open_source_project.update(open_source_project_params)
+      render json: @open_source_project
+    else
+      render json: @open_source_project.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @open_source_project.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def set_open_source_project
+    @open_source_project = @company.open_source_projects.find(params[:id])
+  end
+
+  def open_source_project_params
+    params.require(:open_source_project).permit(:name, :description, :project_type, :url, :icon_url, :stars, :forks)
   end
 end
