48c510a0d9a3e42715d655f7d2610539f50be7c3
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-04 11:16:28 -0700

    Adds open_source validations and scopes

--- app/models/open_source_project.rb
+++ app/models/open_source_project.rb
@@ -1,3 +1,14 @@
 class OpenSourceProject < ApplicationRecord
   belongs_to :company
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :project_type, presence: true, inclusion: { in: %w[contribution sponsorship] }
+  validates :url, presence: true, url: true
+  validates :icon_url, url: true, allow_blank: true
+  validates :stars, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :forks, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  scope :contributions, -> { where(project_type: 'contribution') }
+  scope :sponsorships, -> { where(project_type: 'sponsorship') }
 end
