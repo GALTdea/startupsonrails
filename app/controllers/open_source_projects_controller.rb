05a4567372f97e516b9b3b19826dd0482c5178d1
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-05 12:21:01 -0700

    refactore opensorce model to use appropiate var

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,29 +1,29 @@
 class OpenSourceProjectsController < ApplicationController
   before_action :set_company
-  before_action :authorize_user, only: [:create]
 
   def create
-    @project = @company.open_source_projects.build(open_source_project_params)
-    authorize @project
-    if @project.save
-      render json: @project, status: :created
+    @open_source_project = @company.open_source_projects.build(open_source_project_params)
+
+    if @open_source_project.save
+      render json: @open_source_project, status: :created
     else
-      render json: { errors: @project.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @open_source_project.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def set_company
-    # @company = current_user.company
-    @company = Company.friendly.find(params[:company_id])
+    @company = if params[:company_id]
+                 Company.find_by!(slug: params[:company_id])
+               elsif current_user.company
+                 current_user.company
+               else
+                 raise ActiveRecord::RecordNotFound, 'Company not found'
+               end
   end
 
   def open_source_project_params
     params.require(:open_source_project).permit(:url, :project_type)
-  end
-
-  def authorize_user
-    authorize OpenSourceProject
   end
 end
