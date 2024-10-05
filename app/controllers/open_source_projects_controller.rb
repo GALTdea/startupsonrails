e42d09f77200349fa07bba4f8afa648695b996bf
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-05 11:44:13 -0700

    add authorize_user policy to opensourceprogectcontroller

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,8 +1,10 @@
 class OpenSourceProjectsController < ApplicationController
   before_action :set_company
+  before_action :authorize_user, only: [:create]
 
   def create
     @project = @company.open_source_projects.build(open_source_project_params)
+    authorize @project
     if @project.save
       render json: @project, status: :created
     else
@@ -13,10 +15,14 @@ class OpenSourceProjectsController < ApplicationController
   private
 
   def set_company
-    @company = Company.friendly.find(params[:company_id])
+    @company = current_user.company
   end
 
   def open_source_project_params
     params.require(:open_source_project).permit(:url, :project_type)
+  end
+
+  def authorize_user
+    authorize OpenSourceProject
   end
 end
