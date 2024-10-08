0233546b754c07694580dadce2e9161048687b2c
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-08 10:15:39 -0700

    refactores destroy and set_company method in open_source_projects controller, adds before_action for set_project and authorize_user

--- app/controllers/open_source_projects_controller.rb
+++ app/controllers/open_source_projects_controller.rb
@@ -1,5 +1,7 @@
 class OpenSourceProjectsController < ApplicationController
   before_action :set_company
+  before_action :set_project, only: [:destroy]
+  before_action :authorize_user, only: [:destroy]
 
   def create
     @open_source_project = @company.open_source_projects.build(open_source_project_params)
@@ -11,16 +13,28 @@ class OpenSourceProjectsController < ApplicationController
     end
   end
 
+  def destroy
+    if @project.destroy
+      render json: { success: true }, status: :ok
+    else
+      render json: { success: false }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_company
-    @company = if params[:company_id]
-                 Company.find_by!(slug: params[:company_id])
-               elsif current_user.company
-                 current_user.company
-               else
-                 raise ActiveRecord::RecordNotFound, 'Company not found'
-               end
+    @company = Company.friendly.find(params[:company_id])
+  end
+
+  def set_project
+    @project = @company.open_source_projects.find(params[:id])
+  end
+
+  def authorize_user
+    return if current_user && (current_user.admin? || current_user == @company.user)
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
   end
 
   def open_source_project_params
