920314c1e7f785eb07ec0a6c97eadfbcd8f62e6a
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-10-29 11:38:03 -0700

    adds status enum to issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,4 +3,6 @@ class Issue < ApplicationRecord
   has_one :company, through: :open_source_project
 
   attr_accessor :github_issue
+
+  enum status: { pending: 0, active: 1, resolved: 2, closed: 3 }
 end
