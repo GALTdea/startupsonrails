8fd902cd9e2dd8d2466eb6417132449228ae1d85
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2024-11-06 13:28:26 -0800

    feat: add project support management and flash messages

--- app/controllers/project_supports_controller.rb
+++ app/controllers/project_supports_controller.rb
@@ -1,5 +1,7 @@
 class ProjectSupportsController < ApplicationController
   before_action :set_company, only: %i[index new create]
+  before_action :set_project_support, only: [:destroy]
+  before_action :authorize_destroy, only: [:destroy]
 
   def index
     @project_supports = @company.project_supports.includes(:open_source_project)
@@ -32,10 +34,65 @@ class ProjectSupportsController < ApplicationController
     end
   end
 
+  def destroy
+    @company = @project_support.company
+    project_name = @project_support.open_source_project.url.split('/').last
+    support_type = @project_support.support_type
+
+    if @project_support.destroy
+      success_message = "#{project_name} has been removed from #{support_type} projects"
+
+      respond_to do |format|
+        format.html do
+          redirect_to company_project_supports_path(@company),
+                      notice: success_message
+        end
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.multi(
+            turbo_stream.remove("project_support_#{@project_support.id}"),
+            turbo_stream.append('flash',
+                                partial: 'shared/flash',
+                                locals: {
+                                  message: success_message,
+                                  type: 'success'
+                                })
+          )
+        end
+      end
+    else
+      error_message = 'Unable to remove project support'
+      respond_to do |format|
+        format.html do
+          redirect_to company_project_supports_path(@company),
+                      alert: error_message
+        end
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append('flash',
+                                                   partial: 'shared/flash',
+                                                   locals: {
+                                                     message: error_message,
+                                                     type: 'danger'
+                                                   })
+        end
+      end
+    end
+  end
+
   private
 
   def set_company
     @company = Company.friendly.find(params[:company_id])
+  end
+
+  def set_project_support
+    @project_support = ProjectSupport.find(params[:id])
+  end
+
+  def authorize_destroy
+    return if current_user&.admin? || current_user == @project_support.company.user
+
+    redirect_to company_project_supports_path(@project_support.company),
+                alert: 'You are not authorized to perform this action'
   end
 
   def project_support_params
